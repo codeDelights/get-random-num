69c3e0842bfde58b20e22945b99f099a2734d144
Karthikeyan <karthikeyanp@ilink-systems.com>
2020-07-03 12:26:46 +0530

    Bug Fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = (max = 10, min = 1) => {
-    /* min = Math.ceil(min); 
-    max = Math.floor(max); */ 
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
